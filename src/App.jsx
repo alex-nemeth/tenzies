b5645f007495efa90a65f37068a53ceb29e97602
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-02 23:30:02 +0200

    holdDice func init

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
 
 export default function App() {
     const [dice, setDice] = useState([]);
@@ -10,13 +11,22 @@ export default function App() {
     }, []);
 
     const diceArray = dice.map((die, i) => {
-        return <Die value={die} key={i} />;
+        return <Die die={die} key={nanoid()} handleClick={holdDice} />;
     });
 
     function allNewDice() {
         const arr = [];
-        while (arr.length < 10) arr.push(Math.ceil(Math.random() * 6));
+        while (arr.length < 10)
+            arr.push({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid(),
+            });
         setDice(arr);
+    }
+
+    function holdDice(id) {
+        console.log(id);
     }
 
     return (
---
+++ src/components/Die.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+export default function Die(props) {
+    return (
+        <div
+            className={`die--face ${props.die.isHeld && "held"}`}
+            onClick={() => props.handleClick(props.die.id)}
+        >
+            <h2 className="die--num">{props.die.value}</h2>
+        </div>
+    );
+}
