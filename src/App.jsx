124fa82c82e28edb01d5488968e36fc697b6077f
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-03 00:03:12 +0200

    holdDice map

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,18 @@ export default function App() {
         return <Die die={die} key={nanoid()} handleClick={holdDice} />;
     });
 
+    /**
+     * Challenge: Update the `holdDice` function to flip
+     * the `isHeld` property on the object in the array
+     * that was clicked, based on the `id` prop passed
+     * into the function.
+     *
+     * Hint: as usual, there's > 1 way to accomplish this.
+     * I'll be using `dice.map()` and checking for the `id`
+     * of the die to determine which one to flip `isHeld` on,
+     * but you can do whichever way makes the most sense to you.
+     */
+
     function allNewDice() {
         const arr = [];
         while (arr.length < 10)
@@ -26,7 +38,12 @@ export default function App() {
     }
 
     function holdDice(id) {
-        console.log(id);
+        setDice((prevState) =>
+            prevState.map((die) => {
+                if (die.id == id) die.isHeld = !die.isHeld;
+                return die;
+            })
+        );
     }
 
     return (
