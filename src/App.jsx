1b75d7b8ee31611b9ad902ab122fb7249f3a04e0
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-03 10:19:02 +0200

    pb localstorage & conditional confetti

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,13 @@ import Confetti from "react-confetti";
 export default function App() {
     const [dice, setDice] = useState(allNewDice());
     const [tenzies, setTenzies] = useState(false);
-
-    useEffect(() => {
-        allNewDice();
-    }, []);
+    const [count, setCount] = useState(0);
+    const [personalBest, setPersonalBest] = useState(
+        localStorage.getItem("pb") ? localStorage.getItem("pb") : 999
+    );
+    const [prevPB, setPrevPB] = useState(
+        localStorage.getItem("prevPB") ? localStorage.getItem("prevPB") : 999
+    );
 
     useEffect(() => {
         const heldValue = dice[0].value;
@@ -18,6 +21,15 @@ export default function App() {
         const allSameValue = dice.every((die) => die.value === heldValue);
         if (allHeld && allSameValue) {
             setTenzies(true);
+            if (personalBest > count) {
+                setPrevPB(personalBest);
+                setPersonalBest(count);
+                localStorage.setItem("pb", count);
+                localStorage.setItem("prevPB", prevPB);
+            } else {
+                setPrevPB(personalBest);
+                localStorage.setItem("prevPB", prevPB);
+            }
         }
     }, [dice]);
 
@@ -41,6 +53,7 @@ export default function App() {
                 return die.isHeld ? die : generateNewDie();
             })
         );
+        setCount(count + 1);
     }
 
     function holdDice(id) {
@@ -54,27 +67,39 @@ export default function App() {
     function restartGame() {
         setDice(allNewDice());
         setTenzies(false);
+        setCount(0);
     }
 
     const diceArray = dice.map((die, i) => {
         return <Die die={die} key={nanoid()} handleClick={holdDice} />;
     });
 
+    const instructions = () => {};
+
     return (
         <main>
-            {tenzies && <Confetti />}
+            {tenzies && personalBest < prevPB && <Confetti />}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">
-                Roll until all dice are the same. Click each die to freeze it at
-                its current value between rolls.
+                {count > 0 && "Rolls: " + count}
+                <br />
+                {personalBest < 999
+                    ? "Personal Best: " + personalBest
+                    : count === 0 &&
+                      "Roll until all dice are the same. Click each die to freeze it at its current value between rolls."}
+                <br />
+                {tenzies &&
+                    personalBest < prevPB &&
+                    "Congratulations! New personal best!"}
             </p>
             <div className="die--container">{diceArray}</div>
             <button
                 className="die--reroll"
                 onClick={tenzies ? restartGame : rollDice}
             >
-                {tenzies ? "New Game" : "Roll"}
+                {tenzies ? "Play Again" : "Roll"}
             </button>
+            <p></p>
         </main>
     );
 }
